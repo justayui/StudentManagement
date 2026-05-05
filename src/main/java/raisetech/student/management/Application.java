5ebe270d0bd62ba5a0212d3fdf2b80f4b15ec558
justayui <rina.s2.yui@gmail.com>
2026-05-05 17:37:40 +0900

    add.apache.commons

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,5 +1,7 @@
 package raisetech.student.management;
 
+import ch.qos.logback.core.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
