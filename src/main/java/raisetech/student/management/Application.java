c0aab15c7a1e67c995f3f82d88271588a61948cc
justayui <rina.s2.yui@gmail.com>
2026-05-06 13:51:54 +0900

    Map追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,22 +1,35 @@
 package raisetech.student.management;
 
-import ch.qos.logback.core.util.StringUtil;
-import org.apache.commons.lang3.StringUtils;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Rina Tanaka";
+	private String age = "3";
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+			SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello,world";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age+"歳";
 	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(@RequestBody Map<String,String>student){
+   this.name = student.get("name");
+	 this.age = student.get("age");
+	}
+
+
+
 }
