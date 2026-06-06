6902b418ac7f3b4e34a6bd7b84e2b80e27b066af
justayui <rina.s2.yui@gmail.com>
2026-06-07 00:34:27 +0900

    StudentCoursesクラスの作成及びStudentとStudentsCoursesのRead処理実装

--- src/main/java/raisetech/student/management/Student.java
+++ src/main/java/raisetech/student/management/Student.java
@@ -1,25 +1,17 @@
 package raisetech.student.management;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+  private int id;
   private String name;
   private int age;
-
-  public Student() {
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String nameKana;
+  private String nickname;
+  private String email;
+  private String gender;
+  private String placeOfResidence;
 }
