6cbc9a8ab2a308d25ffb8c1596ab3a6dd1038768
justayui <rina.s2.yui@gmail.com>
2026-06-07 17:22:03 +0900

    controllerパッケージへのconverterの追加。 Studentテーブルへの備考欄と論理削除機能の追加

---
+++ src/main/java/raisetech/student/management/Controller/converter/StudentConverter.java
@@ -0,0 +1,29 @@
+package raisetech.student.management.Controller.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.domain.StudentDetail;
+
+@Component
+public class StudentConverter {
+
+  public @NonNull List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<StudentsCourses> studentsCourses) {
+    List<StudentDetail> studentDetails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
+          .filter(studentsCourse -> student.getId() == studentsCourse.getStudentId())
+          .collect(Collectors.toList());
+      studentDetail.setStudentsCourses(convertStudentCourses);
+      studentDetails.add(studentDetail);
+    });
+    return studentDetails;
+  }
+}
