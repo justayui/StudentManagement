5e9ba9a6661b9732cbb852b660608c2d1950c6b9
justayui <rina.s2.yui@gmail.com>
2026-06-07 14:53:37 +0900

    パッケージの整理とServiceクラスへの抽出処理の実装

---
+++ src/main/java/raisetech/student/management/Service/StudentService.java
@@ -0,0 +1,33 @@
+package raisetech.student.management.Service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList(){
+  List<Student> studentList = repository.search().stream()
+      .filter(student -> student.getAge()>=20 && student.getAge()<30)
+      .toList();
+    return studentList;
+  }
+
+  public List<StudentsCourses> searchStudentCourseList() {
+  List<StudentsCourses> studentsCoursesList = repository.searchCourse().stream()
+      .filter(studentsCourses -> "Javaフルコース".equals(studentsCourses.getCourseName()))
+      .toList();
+    return studentsCoursesList;
+  }
+}
