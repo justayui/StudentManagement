5af949533c054876d7399195148a973554c86efe
justayui <rina.s2.yui@gmail.com>
2026-06-23 12:36:56 +0900

    ＜課題33＞ Read処理のREST化とPostman実践

--- src/main/java/raisetech/student/management/Controller/StudentController.java
+++ src/main/java/raisetech/student/management/Controller/StudentController.java
@@ -1,23 +1,19 @@
 package raisetech.student.management.Controller;
 
 import java.util.Arrays;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.Controller.converter.StudentConverter;
 import raisetech.student.management.Service.StudentService;
-import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentsCourses;
 import raisetech.student.management.domain.StudentDetail;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -27,13 +23,6 @@ public class StudentController {
   public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
     this.converter = converter;
-  }
-
-  @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList(){
-    List<Student> students = service.searchStudentList();
-    List<StudentsCourses> studentsCourses = service.searchStudentCourseList();
-    return converter.convertStudentDetails(students, studentsCourses);
   }
 
   @GetMapping("/newStudent")
@@ -51,11 +40,5 @@ public class StudentController {
     }
     service.registerStudent(studentDetail);
     return "redirect:/studentList";
-  }
-
-  @PostMapping("/updateStudent")
-  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
-    service.updateStudent(studentDetail);
-    return ResponseEntity.ok("更新処理が成功しました");
   }
 }
---
+++ src/main/java/raisetech/student/management/Controller/StudentRestController.java
@@ -0,0 +1,39 @@
+package raisetech.student.management.Controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.student.management.Controller.converter.StudentConverter;
+import raisetech.student.management.Service.StudentService;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.domain.StudentDetail;
+
+@RestController
+public class StudentRestController {
+  private StudentService service;
+  private StudentConverter converter;
+
+  @Autowired
+  public StudentRestController(StudentService service, StudentConverter converter) {
+    this.service = service;
+    this.converter = converter;
+  }
+
+  @GetMapping("/studentList")
+  public List<StudentDetail> getStudentList(){
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentCourseList();
+    return converter.convertStudentDetails(students, studentsCourses);
+  }
+
+  @PostMapping("/updateStudent")
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
+    service.updateStudent(studentDetail);
+    return ResponseEntity.ok("更新処理が成功しました");
+  }
+}
