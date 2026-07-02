cc355818bda86744eaf7e62a1999f11ae63bc518
justayui <rina.s2.yui@gmail.com>
2026-07-02 22:19:00 +0900

    JPA注釈の整合性担保

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -1,5 +1,6 @@
 package raisetech.student.management.data;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Student {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
