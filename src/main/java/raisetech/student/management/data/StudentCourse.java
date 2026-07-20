71711ca70b23d2b11db2a717f601ff91eb4c5e5c
justayui <rina.s2.yui@gmail.com>
2026-07-20 20:12:03 +0900

    JPA注釈の除去

--- src/main/java/raisetech/student/management/data/StudentCourse.java
+++ src/main/java/raisetech/student/management/data/StudentCourse.java
@@ -2,11 +2,6 @@ package raisetech.student.management.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,16 +9,12 @@ import lombok.Setter;
 @Schema(description = "受講生コース情報")
 @Getter
 @Setter
-@Entity
 public class StudentCourse {
 
     @Schema(description = "受講生コース情報を一意に識別するためのIDです。自動採番されます。",example = "1")
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Schema(description = "受講コース名。",example = "Javaフルコース")
-    @NotBlank(message = "コース名は必須入力です。")
     private String courseName;
 
     @Schema(description = "受講開始日。LocalDateが自動で設定されます。",example = "2026-07-05",accessMode = AccessMode.READ_ONLY)
