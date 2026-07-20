f15270a799acd89a60716ff6336d564bb70d277e
justayui <rina.s2.yui@gmail.com>
2026-07-20 09:17:04 +0900

    Repositoryテストの分離

--- src/test/java/raisetech/student/management/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/student/management/repository/StudentRepositoryTest.java
@@ -2,13 +2,11 @@ package raisetech.student.management.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import raisetech.student.management.data.Student;
-import raisetech.student.management.data.StudentCourse;
 
 @MybatisTest
 class StudentRepositoryTest {
@@ -18,7 +16,6 @@ class StudentRepositoryTest {
   @Autowired
   private StudentCourseRepository courseRepository;
 
-  //StudentRepository.Test
   @Test
   void 受講生の全件検索が行えること(){
     List<Student> actual = sut.search();
@@ -70,54 +67,5 @@ class StudentRepositoryTest {
     assertThat(actual.getAge()).isEqualTo(20);
   }
 
-  //StudentCourseRepository.Test
-  @Test
-  void 受講生コース情報が全件検索できること(){
-    List<StudentCourse> actual = courseRepository.searchCourse();
-    assertThat(actual).hasSize(9);
-  }
 
-  @Test
-  void 受講生IDに紐づく受講生コース情報の検索ができること(){
-    List<StudentCourse> actual = courseRepository.searchStudentCourse(1);
-
-    assertThat(actual).hasSize(2);
-    assertThat(actual).extracting("courseName").containsExactlyInAnyOrder("Javaフルコース","AWS基礎コース");
-  }
-
-  @Test
-  void 受講生コース情報の登録ができること(){
-    StudentCourse course = new StudentCourse();
-    course.setCourseName("Javaフルコース");
-    course.setStartDate(LocalDate.now());
-    course.setEndDate(LocalDate.now().plusYears(1));
-
-    courseRepository.registerCourse(course);
-
-    List<StudentCourse> actual = courseRepository.searchCourse();
-    assertThat(actual).hasSize(10);
-  }
-
-  @Test
-  void 受講生コースの更新ができること(){
-    List<StudentCourse> courseList =courseRepository.searchStudentCourse(1);
-    assertThat(courseList).hasSize(2);
-
-    StudentCourse targetCourse = null;
-    for(StudentCourse course : courseList){
-      if("Javaフルコース".equals(course.getCourseName())){
-        targetCourse = course;
-        break;
-      }
-    }
-
-    assertThat(targetCourse).isNotNull();
-
-    targetCourse.setCourseName("Web開発コース");
-    courseRepository.updateStudentCourse(targetCourse);
-
-    List<StudentCourse> actual = courseRepository.searchStudentCourse(1);
-    assertThat(actual).hasSize(2);
-    assertThat(actual).extracting("courseName").containsExactlyInAnyOrder("Web開発コース","AWS基礎コース");
-  }
 }
