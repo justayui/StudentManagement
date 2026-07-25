3a5bc2f4d195e97f7230f9cb842665ce5d194b00
justayui <rina.s2.yui@gmail.com>
2026-07-25 20:52:11 +0900

    EnumクラスへのJavaDocとスキーマの追記

--- src/main/java/raisetech/student/management/data/enums/EnumCourseName.java
+++ src/main/java/raisetech/student/management/data/enums/EnumCourseName.java
@@ -1,11 +1,21 @@
 package raisetech.student.management.data.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
+/**
+ * 受講コース名を表すEnumです
+ */
+@Schema(description = "受講コース名", enumAsRef = true)
 @Getter
 public enum EnumCourseName {
+  @Schema(description = "Javaフルコース")
   JAVA_FULL("Javaフルコース"),
-  AWS("AWSコース"),
+
+  @Schema(description = "AWS基礎コース")
+  AWS("AWS基礎コース"),
+
+  @Schema(description = "Web開発コース")
   WEB_DEVELOPMENT("Web開発コース");
 
   private final String label;
